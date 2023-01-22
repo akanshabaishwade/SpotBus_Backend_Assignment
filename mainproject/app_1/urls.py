f632e92e1d7d96602953547c62dca4397937188f
akanshabaishwade <akanshabaishwade@gmail.com>
2023-01-22 19:12:33 +0530

    [Add] login , and profile details

--- mainproject/app_1/urls.py
+++ mainproject/app_1/urls.py
@@ -5,5 +5,7 @@ from .api import *
 urlpatterns = [
     # After register call (api/token/) to get token
     path('register/', RegisterApi.as_view()),
+    path('login/', LoginView.as_view()),
+    path('profile/', ProfileView.as_view()),
 
 ]
